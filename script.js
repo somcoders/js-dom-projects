43a1d943e8a55196d6dcc5ca758790261e690b7c
Somcoders <faahid44@gmail.com>
2021-03-26 11:42:33 +0300

    Create element lesson.

---
+++ script.js
@@ -0,0 +1,10 @@
+let container   = document.querySelector(".container");
+
+let subBtn  =   document.createElement("button");
+    subBtn.innerHTML       = "Nala Soco";
+    subBtn.style.background = "orange";
+    subBtn.id =  "subBtn";
+
+    container.appendChild(subBtn);
+
+    // container.removeChild(subBtn);
